7ca19017bac2de658fd74272cb1478e7f4831844
Santiago Facchini <santiago.facchini@icloud.com>
2022-07-04 17:00:38 -0300

    Switched from HTML parsing using bs4 to hidden API

--- ni-gazette.py
+++ ni-gazette.py
@@ -1,8 +1,7 @@
-import re
 import os
 import ftplib
 import requests
-import bs4
+import urllib.parse
 import PyPDF2
 import ghostscript
 
@@ -20,65 +19,85 @@ print(ftp_connection.getwelcome())
 ftp_files = ftp_connection.nlst('/DO_Nicaragua/procesados/')
 ftp_connection.quit()
 
-# Get URL
-seed_url = 'http://digesto.asamblea.gob.ni/?project=la-gaceta-diario-oficial-2021'
-response = requests.get(seed_url)
-print(seed_url, response)
+# URL
+seed_url = 'http://digesto.asamblea.gob.ni/consultas/util/ws/proxy.php'
 
-# Parse response
-soup_object = bs4.BeautifulSoup(response.content, 'html.parser')
-issue_pattern = re.compile(r'La Gaceta, Diario Oficial N°. (\d+) del (\d+)/(\d+)/(\d+)')
-issues = soup_object.find_all('a', text=issue_pattern)
-print(f'Issues in FTP: {len(issues)}')
+# 2022
+payload = {
+    'hddQueryType': 'getRdds',
+    'txtDatePublishFrom': '2022/1/1',
+    'txtDatePublishTo': '2022/12/31'
+}
 
-for issue in issues:
+response = requests.post(
+    url=seed_url,
+    data=payload
+)
 
-    # Construct file name
-    mo = re.search(issue_pattern, issue.text.strip())
-    file_name = f'{mo.group(1).zfill(2)}_{mo.group(2)}{mo.group(3)}{mo.group(4)}'
+for issue in response.json()['rdds']:
+    
+    # JSON response data
+    issue_date = issue['fecPublica']
+    issue_number = issue['numPublica']
+    issue_rddid = issue['rddid']
+
+    # vLex name format
+    vlex_name = f'{issue_number}_{issue_date.replace("/", "")}'
 
     # If file exists in FTP
-    if f'{file_name}.pdf' in ftp_files and f'{file_name}.csv' in ftp_files:
+    if f'{vlex_name}.pdf' in ftp_files and f'{vlex_name}.csv' in ftp_files:
         try:
-            os.remove(f'{download_directory}{file_name}.csv')
-            os.remove(f'{download_directory}{file_name}.pdf')
-            print(f'{file_name} already in FTP. Skipped.')
+            os.remove(f'{download_directory}{vlex_name}.csv')
+            os.remove(f'{download_directory}{vlex_name}.pdf')
+            print(f'{vlex_name} already in FTP. Skipped.')
         except:
-            print(f'{file_name} already in FTP. Skipped.')
+            print(f'{vlex_name} already in FTP. Skipped.')
 
     # If file is neither in FTP nor in $HOME
-    elif f'{file_name}.csv' not in ftp_files and not os.path.isfile(f'{download_directory}{file_name}.pdf'):
-        print(f'Working on {file_name}: {issue["href"]}')
+    elif f'{vlex_name}.csv' not in ftp_files and not os.path.isfile(f'{download_directory}{vlex_name}.pdf'):
+        print(f'Working on {vlex_name}: {issue_rddid}')
 
-        pdf_content = requests.get(issue['href']).content
+        # GET query params
+        query_params = {
+            'type': 'rdd',
+            'rdd': urllib.parse.quote(issue_rddid, safe=''), # Escape special characters
+        }
+        
+        # Bytes response -> PDF content
+        pdf_content = requests.get(
+            url=f'http://digesto.asamblea.gob.ni/consultas/util/pdf.php',
+            data=query_params,
+        ).content
 
-        # Create PDF file
-        with open(f'{download_directory}raw-{file_name}.pdf', 'wb') as pdf_file:
+        # Create raw PDF file
+        with open(f'{download_directory}raw-{vlex_name}.pdf', 'wb') as pdf_file:
             pdf_file.write(pdf_content)
 
         # Count PDF pages
-        read_pdf = PyPDF2.PdfFileReader(f'{download_directory}raw-{file_name}.pdf')
+        read_pdf = PyPDF2.PdfFileReader(f'{download_directory}raw-{vlex_name}.pdf')
         total_pages = read_pdf.numPages
 
-        # Ghostscript
+        # Ghostscript -> Create PDF/A file
         args = [
             '-dPDFA=1',
             '-dBATCH',
             '-dNOPAUSE',
             '-sColorConversionStrategy=RGB',
             '-sDEVICE=pdfwrite',
-            f'-sOutputFile={download_directory}{file_name}.pdf',
-            f'{download_directory}raw-{file_name}.pdf',
+            f'-sOutputFile={download_directory}{vlex_name}.pdf',
+            f'{download_directory}raw-{vlex_name}.pdf',
         ]
         ghostscript.Ghostscript(*args)
-        os.remove(f'{download_directory}raw-{file_name}.pdf')
+
+        # Remove raw PDF file
+        os.remove(f'{download_directory}raw-{vlex_name}.pdf')
 
         # Create CSV file
-        with open(f'{download_directory}{file_name}.csv', 'w') as csv_file:
-            csv_file.write(f'La Gaceta, Diario Oficial Nº {mo.group(1)} del '
-                f'día {mo.group(2)}/{mo.group(3)}/{mo.group(4)} (contenido '
-                f'completo)||Contenido completo|{mo.group(2)}/{mo.group(3)}/'
-                f'{mo.group(4)}|1|{total_pages}')
+        with open(f'{download_directory}{vlex_name}.csv', 'w') as csv_file:
+            csv_file.write(f'La Gaceta, Diario Oficial Nº {issue_number} del '
+            f'día {issue_date} (contenido completo)||Contenido completo|'
+            f'{issue_date}|1|{total_pages}'
+        )
 
     else:
-        print(f'{file_name} already in {download_directory}. Skipped.')
+        print(f'{vlex_name} already in {download_directory}. Skipped.')
